0ab650e9e33c04b76c0efcc98d62037b91ab7084
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-19 23:31:23 +0800

    basic post creation added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,32 @@ class PostsController < ApplicationController
     before_action :authenticate_user!
 
     def index
-        @post = Post.all
+        @posts = Post.all
     end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = current_user.posts.build(post_params)
+
+        if @post.save
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    private 
+
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
+
 end
