364b055445e8d91973faffbae9ad9e12fd3b4c7e
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-19 23:41:39 +0800

    destroy option added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,6 +38,13 @@ class PostsController < ApplicationController
         end
     end
 
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+    
+        redirect_to root_path, status: :see_other
+      end
+
     private 
 
     def post_params
