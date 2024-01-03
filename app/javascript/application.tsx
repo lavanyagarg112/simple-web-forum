a8179859e05d128cf941de1c0dfce4f7a65e4edc
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2024-01-03 21:06:10 +0530

    it works git add .

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+
+// import React from 'react';
+
+// import App from "packs/application";
+
+// const root = createRoot(document.getElementById('root'));
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
